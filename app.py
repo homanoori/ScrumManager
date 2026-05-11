63c1fcf3dcca0d36082d5b713ffb8e439524bfe1
Atena <atena.hosseinifarr@gmail.com>
2026-05-11 15:34:15 -0400

    temp: add seed route for demo data

--- app.py
+++ app.py
@@ -54,5 +54,11 @@ def index():
 with app.app_context():
     db.create_all()
     
+@app.route("/seed-demo-data")
+def seed_demo():
+    from seed import seed
+    seed()
+    return "Seeded!"
+
 if __name__ == "__main__":
     app.run(debug=True)
